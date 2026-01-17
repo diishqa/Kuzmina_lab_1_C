dc343d15f86187a439e577bede55183e02139c79
diishqa <kkover073@gmail.com>
2026-01-17 16:27:34 +0300

    задание 2

--- backend/program.cs
+++ backend/program.cs
@@ -2,3 +2,18 @@
 Console.WriteLine("Привет!");
 Console.WriteLine("Кузьмина Диана Александровна");
 Console.WriteLine(DateTime.Now);
+
+bool fl=true;
+while (fl){
+    Console.Write("Введите имя:");
+    string name= Console.ReadLine();
+    Console.WriteLine($"Привет, {name}!");
+    Console.WriteLine($"Сегодня -{DateTime.Now.DayOfWeek}");
+    Console.Write("Хотите продолжить? (y/n): ");
+    string otv=Console.ReadLine();
+    if (otv != "y")
+    {
+        Console.WriteLine("Программа закончена");
+        fl=false;
+    }
+}
